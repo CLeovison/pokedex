ffee7cb84e389df392d31a8ac389a4723fe9fd95
Leovison <cleovison@gmail.com>
2024-01-31 10:05:13 +0800

    A

--- src/pages/PokeData.jsx
+++ src/pages/PokeData.jsx
@@ -6,31 +6,40 @@ export default function PokeData() {
     id: "",
   });
   const [dataInput, setDataInput] = useState("");
-
+  const [loading, setLoading] = useState(false);
+  const [pokemon, setPokemon] = useState("");
   useEffect(() => {
     const fetching = async () => {
       try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
-        const json = response.json();
-        setDataInput(json);
+        setLoading(true);
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${dataInput}`
+        );
+        if (!response.ok) {
+          throw new Error("Error encountered. Please Fuck Again");
+        }
+        const json = await response.json();
+        setPokemonData(json);
+        setLoading(false);
       } catch (err) {
         console.log("The Data Was Not Fetch");
       }
     };
     fetching();
-  }, [pokemonData]);
-
+  }, [dataInput]);
+  console.log(pokemonData);
   //Handler
 
-  function inputHandler() {
-    setDataInput(e.target.value);
-  }
   return (
     <>
       <form action="">
         <label htmlFor="">
           Pokemon Name:
-          <input type="text" onChange={(e) => inputHandler(e)} />
+          <input
+            type="text"
+            value={dataInput}
+            onChange={(e) => setDataInput(e.target.value)}
+          />
         </label>
 
         <button type="submit">Submit</button>
