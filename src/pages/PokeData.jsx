350c0636878bee0d32c2210e5e7d2a24e426ccd8
Leovison <cleovison@gmail.com>
2024-02-01 18:12:53 +0800

    To Lowercase problem

--- src/pages/PokeData.jsx
+++ src/pages/PokeData.jsx
@@ -1,48 +1,50 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export default function PokeData() {
-  const [pokemonData, setPokemonData] = useState({
-    name: "",
-    id: "",
-  });
-  const [dataInput, setDataInput] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [pokemon, setPokemon] = useState("");
-  useEffect(() => {
-    const fetching = async () => {
-      try {
-        setLoading(true);
-        const response = await fetch(
-          `https://pokeapi.co/api/v2/pokemon/${dataInput}`
-        );
-        if (!response.ok) {
-          throw new Error("Error encountered. Please Fuck Again");
-        }
-        const json = await response.json();
-        setPokemonData(json);
-        setLoading(false);
-      } catch (err) {
-        console.log("The Data Was Not Fetch");
+  const [isLoading, setIsLoading] = useState(false);
+  const [datainput, setDataInput] = useState("");
+  const [pokemonData, setPokemonData] = useState("");
+
+  const fetching = async (e) => {
+    setPokemonData("");
+    e.preventDefault();
+    try {
+      setIsLoading(true);
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${datainput.toLocaleLowerCase()}`
+      );
+      const json = await response.json();
+      if (!response.ok) {
+        throw new Error("The data was not been fetch, please fuck again");
       }
-    };
-    fetching();
-  }, [dataInput]);
-  console.log(pokemonData);
-  //Handler
+      console.log(json) 
+      setPokemonData(json);
+    } catch (error) {
+      console.log(`There is a error`);
+    }
+    setIsLoading(false);
+  };
 
   return (
     <>
       <form action="">
         <label htmlFor="">
+          {" "}
           Pokemon Name:
           <input
             type="text"
-            value={dataInput}
+            name=""
+            id=""
             onChange={(e) => setDataInput(e.target.value)}
           />
         </label>
-
-        <button type="submit">Submit</button>
+        <button type="submit" onClick={fetching}>
+          Submit
+        </button>
+        <p>{isLoading ? "...Loading" : ""}</p>
+        <img src={pokemonData?.sprites?.front_default} alt="" srcset="" />
       </form>
     </>
   );
